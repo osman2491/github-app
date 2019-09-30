524b0cddcad1dc6ca2d3d41f1634a729415dccba
osman67239121@gmail.com <osman67239121@gmail.com>
2019-09-30 12:12:37 +0300

    added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
